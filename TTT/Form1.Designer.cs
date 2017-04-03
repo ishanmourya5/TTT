501aa091bfa1f8c2c4072267894b4f71d35ee178
Ishan Mourya <Ishan Mourya>
2017-04-03 16:02:15 +0530

    Add project files.

---
+++ TTT/Form1.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace TTT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.peer_ip_textbox = new System.Windows.Forms.TextBox();
+            this.connect_button = new System.Windows.Forms.Button();
+            this.peer_port_textbox = new System.Windows.Forms.TextBox();
+            this.my_port_textbox = new System.Windows.Forms.TextBox();
+            this.my_ip_textbox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.b1 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b6 = new System.Windows.Forms.Button();
+            this.b7 = new System.Windows.Forms.Button();
+            this.b8 = new System.Windows.Forms.Button();
+            this.b9 = new System.Windows.Forms.Button();
+            this.label_turn = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // peer_ip_textbox
+            // 
+            this.peer_ip_textbox.Location = new System.Drawing.Point(77, 95);
+            this.peer_ip_textbox.Name = "peer_ip_textbox";
+            this.peer_ip_textbox.Size = new System.Drawing.Size(125, 20);
+            this.peer_ip_textbox.TabIndex = 2;
+            // 
+            // connect_button
+            // 
+            this.connect_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.connect_button.Location = new System.Drawing.Point(298, 68);
+            this.connect_button.Name = "connect_button";
+            this.connect_button.Size = new System.Drawing.Size(73, 46);
+            this.connect_button.TabIndex = 3;
+            this.connect_button.Text = "Connect";
+            this.connect_button.UseVisualStyleBackColor = true;
+            this.connect_button.Click += new System.EventHandler(this.connect_button_Click);
+            // 
+            // peer_port_textbox
+            // 
+            this.peer_port_textbox.Location = new System.Drawing.Point(208, 94);
+            this.peer_port_textbox.Name = "peer_port_textbox";
+            this.peer_port_textbox.Size = new System.Drawing.Size(68, 20);
+            this.peer_port_textbox.TabIndex = 5;
+            // 
+            // my_port_textbox
+            // 
+            this.my_port_textbox.Location = new System.Drawing.Point(208, 68);
+            this.my_port_textbox.Name = "my_port_textbox";
+            this.my_port_textbox.Size = new System.Drawing.Size(68, 20);
+            this.my_port_textbox.TabIndex = 6;
+            // 
+            // my_ip_textbox
+            // 
+            this.my_ip_textbox.Location = new System.Drawing.Point(77, 68);
+            this.my_ip_textbox.Name = "my_ip_textbox";
+            this.my_ip_textbox.Size = new System.Drawing.Size(125, 20);
+            this.my_ip_textbox.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(135, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "IP";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(224, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Port";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(36, 71);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Own";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(36, 98);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Peer";
+            // 
+            // b1
+            // 
+            this.b1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b1.Location = new System.Drawing.Point(102, 177);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(60, 60);
+            this.b1.TabIndex = 13;
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // b2
+            // 
+            this.b2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b2.Location = new System.Drawing.Point(168, 177);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(60, 60);
+            this.b2.TabIndex = 13;
+            this.b2.UseVisualStyleBackColor = true;
+            this.b2.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // b3
+            // 
+            this.b3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b3.Location = new System.Drawing.Point(234, 177);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(60, 60);
+            this.b3.TabIndex = 13;
+            this.b3.UseVisualStyleBackColor = true;
+            this.b3.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // b4
+            // 
+            this.b4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b4.Location = new System.Drawing.Point(102, 243);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(60, 60);
+            this.b4.TabIndex = 13;
+            this.b4.UseVisualStyleBackColor = true;
+            this.b4.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // b5
+            // 
+            this.b5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b5.Location = new System.Drawing.Point(168, 243);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(60, 60);
+            this.b5.TabIndex = 13;
+            this.b5.UseVisualStyleBackColor = true;
+            this.b5.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // b6
+            // 
+            this.b6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b6.Location = new System.Drawing.Point(234, 243);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(60, 60);
+            this.b6.TabIndex = 13;
+            this.b6.UseVisualStyleBackColor = true;
+            this.b6.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // b7
+            // 
+            this.b7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b7.Location = new System.Drawing.Point(102, 309);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(60, 60);
+            this.b7.TabIndex = 13;
+            this.b7.UseVisualStyleBackColor = true;
+            this.b7.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // b8
+            // 
+            this.b8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b8.Location = new System.Drawing.Point(168, 309);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(60, 60);
+            this.b8.TabIndex = 13;
+            this.b8.UseVisualStyleBackColor = true;
+            this.b8.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // b9
+            // 
+            this.b9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b9.Location = new System.Drawing.Point(234, 309);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(60, 60);
+            this.b9.TabIndex = 13;
+            this.b9.UseVisualStyleBackColor = true;
+            this.b9.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // label_turn
+            // 
+            this.label_turn.AutoSize = true;
+            this.label_turn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_turn.Location = new System.Drawing.Point(202, 147);
+            this.label_turn.Name = "label_turn";
+            this.label_turn.Size = new System.Drawing.Size(0, 15);
+            this.label_turn.TabIndex = 15;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(407, 24);
+            this.menuStrip1.TabIndex = 16;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.helpToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.helpToolStripMenuItem.Text = "Help";
+            this.helpToolStripMenuItem.Click += new System.EventHandler(this.helpToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.aboutToolStripMenuItem.Text = "About ";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 410);
+            this.Controls.Add(this.label_turn);
+            this.Controls.Add(this.b9);
+            this.Controls.Add(this.b8);
+            this.Controls.Add(this.b7);
+            this.Controls.Add(this.b6);
+            this.Controls.Add(this.b5);
+            this.Controls.Add(this.b4);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.my_ip_textbox);
+            this.Controls.Add(this.my_port_textbox);
+            this.Controls.Add(this.peer_port_textbox);
+            this.Controls.Add(this.connect_button);
+            this.Controls.Add(this.peer_ip_textbox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox peer_ip_textbox;
+        private System.Windows.Forms.Button connect_button;
+        private System.Windows.Forms.TextBox peer_port_textbox;
+        private System.Windows.Forms.TextBox my_port_textbox;
+        private System.Windows.Forms.TextBox my_ip_textbox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button b4;
+        private System.Windows.Forms.Button b5;
+        private System.Windows.Forms.Button b6;
+        private System.Windows.Forms.Button b7;
+        private System.Windows.Forms.Button b8;
+        private System.Windows.Forms.Button b9;
+        private System.Windows.Forms.Label label_turn;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
+
---
+++ TTT/Form1.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace TTT
+{
+    public partial class Form1 : Form
+    {
+        Socket sock;
+        IPAddress peer_ip,my_ip;
+        String my_ob, peer_ob;
+        EndPoint epLocal, epRemote;
+        int peer_no, my_no;
+        byte[] buffer;
+        int turn;
+        int count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+        
+        private void connect_button_Click(object sender, EventArgs e)
+        {
+            //PEER IP
+            peer_ip = IPAddress.Parse(peer_ip_textbox.Text);
+            peer_no = Convert.ToInt32(peer_port_textbox.Text);
+            //MY IP
+            my_ip = IPAddress.Parse(my_ip_textbox.Text);
+            my_no = Convert.ToInt32(my_port_textbox.Text);
+            //SETTING VALUES
+            if (peer_no > my_no)
+            {
+                label_turn.Text = "Friend's Turn";
+                turn = peer_no;
+                my_ob = "X";
+                peer_ob = "O";
+            }
+            else
+            {
+                label_turn.Text = "Your Turn";
+                turn = my_no;
+                my_ob = "O";
+                peer_ob = "X";
+            }
+            //CONNECTING
+            sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            sock.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);         
+            epLocal = new IPEndPoint(my_ip, my_no);
+            sock.Bind(epLocal);
+            epRemote = new IPEndPoint(peer_ip,peer_no);
+            sock.Connect(epRemote);
+            buffer = new byte[1464];
+            sock.BeginReceiveFrom(buffer, 0, buffer.Length,SocketFlags.None, ref epRemote,new AsyncCallback(OperatorCallBack), buffer);
+            connect_button.Text = "Connected";
+            connect_button.Enabled = false;
+        }
+
+        private void OperatorCallBack(IAsyncResult ar)
+        {
+            try
+            {
+                int size = sock.EndReceiveFrom(ar, ref epRemote);
+                if (size > 0)
+                {
+                    byte[] aux = new byte[1464];
+                    aux = (byte[])ar.AsyncState;
+                    ASCIIEncoding enc = new ASCIIEncoding();
+                    string msg = enc.GetString(aux);
+                    set_button(msg);
+                }
+                buffer = new byte[1464];
+                sock.BeginReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref epRemote, new AsyncCallback(OperatorCallBack), buffer);
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.ToString());
+            }
+        }
+
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            count = 0;
+            if (peer_no > my_no)
+            {
+                label_turn.Text = "Friend's Turn";
+                turn = peer_no;
+            }
+            else
+            {
+                label_turn.Text = "Your Turn";
+                turn = my_no;
+            }
+            enableall();
+        }
+
+        private void b1_Click(object sender, EventArgs e)
+        {
+            if (turn == my_no)
+            {
+                Button bt = (Button)sender;
+                ASCIIEncoding enc = new ASCIIEncoding();
+                byte[] msg = new byte[1464];
+                msg = enc.GetBytes(bt.Name);
+                sock.Send(msg);
+                bt.Text = my_ob;
+                bt.Enabled = false;
+                count++;
+                check();
+                turn = peer_no;
+                label_turn.Text = "Friend's Turn";
+            }
+        }
+
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("You and your Friend first connect to same WiFi/Ethernet \nEnter your IP in 'Own IP' Section\nIn case You dont know your IP:\nOpen Cmd and type 'ipconfig', the IPV4 term will be your IP\n" +
+                "Enter a Random 4 Digit Number into 'Own Port'\nShare these with Your Friend and ask him to enter them into his 'Peer IP' and 'Peer Port' Boxes and Vice-Versa");
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Developed by Ishan Mourya, IIEST Shibpur");
+        }
+
+        public void set_button(String s)
+        {
+            s.Trim();
+            char[] ss = s.ToCharArray();
+            if (ss[1]=='1') 
+            {
+                b1.Text = peer_ob;
+                b1.Enabled = false;
+            }
+            else if (ss[1] == '2')
+            {
+                b2.Text = peer_ob;
+                b2.Enabled = false;
+            }
+            else if (ss[1] == '3')
+            {
+                b3.Text = peer_ob;
+                b3.Enabled = false;
+            }
+            else if (ss[1] == '4')
+            {
+                b4.Text = peer_ob;
+                b4.Enabled = false;
+            }
+            else if (ss[1] == '5')
+            {
+                b5.Text = peer_ob;
+                b5.Enabled = false;
+            }
+            else if (ss[1] == '6')
+            {
+                b6.Text = peer_ob;
+                b6.Enabled = false;
+            }
+            else if (ss[1] == '7')
+            {
+                b7.Text = peer_ob;
+                b7.Enabled = false;
+            }
+            else if (ss[1] == '8')
+            {
+                b8.Text = peer_ob;
+                b8.Enabled = false;
+            }
+            else if (ss[1] == '9')
+            {
+                b9.Text = peer_ob;
+                b9.Enabled = false;
+            }
+            count++;
+            if (check() == false) label_turn.Text = "Your Turn";
+            turn = my_no;
+        }
+
+        public bool check()
+        {
+            bool matchover = false;
+            if ((b1.Text == b2.Text) && (b2.Text == b3.Text) && (!b1.Enabled))
+            {
+                matchover = true;
+            }
+            else if ((b4.Text == b5.Text) && (b5.Text == b6.Text) && (!b4.Enabled))
+            {
+                matchover = true;
+            }
+            else if ((b7.Text == b8.Text) && (b8.Text == b9.Text) && (!b7.Enabled))
+            {
+                matchover = true;
+            }
+            else if ((b1.Text == b4.Text) && (b4.Text == b7.Text) && (!b1.Enabled))
+            {
+                matchover = true;
+            }
+            else if ((b2.Text == b5.Text) && (b5.Text == b8.Text) && (!b2.Enabled))
+            {
+                matchover = true;
+            }
+            else if ((b3.Text == b6.Text) && (b6.Text == b9.Text) && (!b3.Enabled))
+            {
+                matchover = true;
+            }
+            else if ((b1.Text == b5.Text) && (b5.Text == b9.Text) && (!b1.Enabled))
+            {
+                matchover = true;
+            }
+            else if ((b3.Text == b5.Text) && (b5.Text == b7.Text) && (!b3.Enabled))
+            {
+                matchover = true;
+            }
+            else if (count >= 9)
+            {
+                MessageBox.Show("Match Draw !!!", "Match Over");
+                disableall();
+            }
+            if (matchover)
+            {
+                String result;
+                if (turn == my_no) result = "You Win !";
+                else result = "You Loose !";
+                MessageBox.Show(result);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void disableall()
+        {
+            foreach (Control c in Controls)
+            {
+                try
+                {
+                    Button b = (Button)c;
+                    b.Enabled = false;
+                }
+                catch { }
+            }
+        }
+
+        public void enableall()
+        {
+            foreach (Control c in Controls)
+            {
+                try
+                {
+                    Button b = (Button)c;
+                    b.Enabled = true;
+                    b.Text = "";
+                }
+                catch { }
+            }
+            connect_button.Enabled = false;
+            connect_button.Text = "Connected";
+        }
+    }
+}
